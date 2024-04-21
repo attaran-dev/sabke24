e75efb04e3df2225a1026fb07928d832d60dfe08
attaran-dev <103171587+attaran-dev@users.noreply.github.com>
2024-04-21 22:46:37 +0330

    add ci/cd

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(
-    ""
+    "mongodb://root:pzUo5KGRpvf33THxXtfoexhJ@siah-kaman.liara.cloud:31914/my-app?authSource=admin"
   );
   return client;
 }
