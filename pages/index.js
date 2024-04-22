d7d72eb1b1ecd081061d3002202212ca3284e712
attaran-dev <103171587+attaran-dev@users.noreply.github.com>
2024-04-22 09:03:52 +0330

    responsive title

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center gap-10 justify-center">
-      <h1 className="text-9xl font-lalezar">سبک ۲۴</h1>
+      <h1 className="text-6xl sm:text-9xl font-lalezar">سبک ۲۴</h1>
 
       <div className="flex flex-row justify-center gap-6">
         <Link className="rounded bg-gray-200 p-1 hover:bg-gray-300" href={"/blog"}>
