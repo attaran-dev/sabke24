ee0181b76ec2db29e12c61ff43c402027fb03f69
attaran-dev <103171587+attaran-dev@users.noreply.github.com>
2024-06-02 16:23:17 +0330

    some improvements

--- components/post-card/post-card.js
+++ components/post-card/post-card.js
@@ -1,24 +1,27 @@
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 function PostCard(props) {
-  const [hovered, setIsHovered] = useState(false);
   const id = props.id;
   const url = props.url;
   const title = props.title;
   const brief = props.brief;
-useEffect(()=>{
-
-}, [hovered,id,url,title,brief])
+  useEffect(() => {}, [id, url, title, brief]);
   return (
     <Link
       href={`/blog/${url}`}
-      onMouseOver={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      className="p-4 rounded-lg border-4 w-24 h-24 sm:w-36 sm:h-36 lg:w-48 lg:h-48 flex items-center justify-center text-center"
+      className=" p-4 rounded-lg border-4 w-24 h-24 sm:w-36 sm:h-36 lg:w-48 lg:h-48 flex items-center justify-center text-center bg-white/75 group"
     >
-      {!hovered && <span className="animate-[fadein_0.5s] text-xs sm:text-md lg:text-lg font-bold">{title}</span>}
-      {hovered && <p className="text-sm animate-[fadein_0.5s] hidden lg:block">{brief}</p>}
+      {
+        <span className="transition duration-300 text-black/100 text-xs sm:text-md lg:text-lg font-bold lg:group-hover:text-black/0">
+          {title}
+        </span>
+      }
+      {
+        <p className="transition duration-300 text-sm absolute text-black/0 lg:group-hover:text-black/100 m-12">
+          {brief}
+        </p>
+      }
     </Link>
   );
 }
