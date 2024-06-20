57f4a2085cafae39aae582ab7a5aa683d1e0a22a
attaran-dev <103171587+attaran-dev@users.noreply.github.com>
2024-06-20 10:53:58 +0330

    book cards responsived

--- components/cards/book-card/book-card.js
+++ components/cards/book-card/book-card.js
@@ -14,7 +14,7 @@ function BookCard(props) {
   return (
     <Link
       href={`/bookshelf/${url}`}
-      className=" p-8 rounded-lg border-4 w-24 h-24 sm:w-36 sm:h-36 lg:w-60 lg:h-96 flex flex-col gap-4 items-center justify-between text-center bg-white/75 group"
+      className=" p-8 rounded-lg border-4 w-48 h-72 lg:w-60 lg:h-96 flex flex-col gap-4 items-center justify-between text-center bg-white/75 group"
     >
       <div>
         <Image src={permanentLink} alt="" width={180} height={180} />
