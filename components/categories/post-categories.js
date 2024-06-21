5379c0b564fa54c98a080075a7ed6a619f4d63d7
attaran-dev <103171587+attaran-dev@users.noreply.github.com>
2024-06-21 18:14:18 +0330

    post categories improved

--- components/categories/post-categories.js
+++ components/categories/post-categories.js
@@ -4,7 +4,7 @@ function PostCategories() {
   return (
     <div
       className="bg-[url('/images/blue-oil-paint-strokes-textured-background-low-saturation.jpg')] bg-cover bg-center
-    grid grid-cols-2 auto-rows-fr gap-y-8 gap-x-16 p-10
+    grid grid-cols-2 auto-rows-fr gap-y-16 gap-x-16 p-10
     md:grid md:grid-cols-5 md:gap-12 md:p-12
      "
     >
@@ -17,7 +17,7 @@ function PostCategories() {
           <div>مدیریت درآمد</div>
           <div className="text-sm font-normal">الگوهای نوین درآمدزایی</div>
         </div>
-        <div className="absolute -bottom-[20%] right-[6%] text-4xl w-10 h-10 bg-white rounded-full">۱</div>
+        <div className="absolute -bottom-6 right-4 text-4xl w-10 h-10 bg-white rounded-full">۱</div>
       </Link>
       <Link
         href="/category/expense"
@@ -28,7 +28,7 @@ function PostCategories() {
           <div>مدیریت هزینه</div>
           <div className="text-sm font-normal">شیوه‌های هزینه و مصرف</div>
         </div>
-      <div className="absolute -bottom-[20%] right-[6%] text-4xl w-10 h-10 bg-white rounded-full">۲</div>
+      <div className="absolute -bottom-6 right-4 text-4xl w-10 h-10 bg-white rounded-full">۲</div>
       </Link>
       <Link
         href="/category/nutrition"
@@ -39,7 +39,7 @@ function PostCategories() {
           <div>مدیریت تغذیه</div>
           <div className="text-sm font-normal">برنامه‌ها و رژیم‌های غذایی</div>
         </div>
-      <div className="absolute -bottom-[20%] right-[6%] text-4xl w-10 h-10 bg-white rounded-full">۳</div>
+      <div className="absolute -bottom-6 right-4 text-4xl w-10 h-10 bg-white rounded-full">۳</div>
       </Link>
       <Link
         href="/category/thought"
@@ -50,7 +50,7 @@ function PostCategories() {
           <div>مدیریت اندیشه</div>
           <div className="text-sm font-normal">الگوهای فکر و اندیشه</div>
         </div>
-      <div className="absolute -bottom-[20%] right-[6%] text-4xl w-10 h-10 bg-white rounded-full">۴</div>
+      <div className="absolute -bottom-6 right-4 text-4xl w-10 h-10 bg-white rounded-full">۴</div>
       </Link>
       <Link
         href="/category/prayer"
@@ -61,7 +61,7 @@ function PostCategories() {
           <div>مدیریت نیایش</div>
           <div className="text-sm font-normal">متدهای دعا و نیایش</div>
         </div>
-      <div className="absolute -bottom-[20%] right-[6%] text-4xl w-10 h-10 bg-white rounded-full">۵</div>
+      <div className="absolute -bottom-6 right-4 text-4xl w-10 h-10 bg-white rounded-full">۵</div>
       </Link>
       <Link
         href="/category/recreation"
@@ -72,7 +72,7 @@ function PostCategories() {
           <div>مدیریت تفریح</div>
           <div className="text-sm font-normal">الگوهای تفریح و بازی</div>
         </div>
-      <div className="absolute -bottom-[20%] right-[6%] text-4xl w-10 h-10 bg-white rounded-full">۶</div>
+      <div className="absolute -bottom-6 right-4 text-4xl w-10 h-10 bg-white rounded-full">۶</div>
       </Link>
       <Link
         href="/category/sport"
@@ -83,7 +83,7 @@ function PostCategories() {
           <div>مدیریت ورزش</div>
           <div className="text-sm font-normal">برنامه‌ها و گونه‌های ورزش</div>
         </div>
-      <div className="absolute -bottom-[20%] right-[6%] text-4xl w-10 h-10 bg-white rounded-full">۷</div>
+      <div className="absolute -bottom-6 right-4 text-4xl w-10 h-10 bg-white rounded-full">۷</div>
       </Link>
       <Link
         href="/category/hygiene"
@@ -94,7 +94,7 @@ function PostCategories() {
           <div>مدیریت بهداشت</div>
           <div className="text-sm font-normal">الگوها و برنامه‌های بهداشتی</div>
         </div>
-      <div className="absolute -bottom-[20%] right-[6%] text-4xl w-10 h-10 bg-white rounded-full">۸</div>
+      <div className="absolute -bottom-6 right-4 text-4xl w-10 h-10 bg-white rounded-full">۸</div>
       </Link>
       <Link
         href="/category/communication"
@@ -107,7 +107,7 @@ function PostCategories() {
             الگوهای ازدواج و مهارت‌های ارتباط
           </div>
         </div>
-      <div className="absolute -bottom-[20%] right-[6%] text-4xl w-10 h-10 bg-white rounded-full">۹</div>
+      <div className="absolute -bottom-6 right-4 text-4xl w-10 h-10 bg-white rounded-full">۹</div>
       </Link>
       <Link
         href="/category/sleep"
@@ -118,7 +118,7 @@ function PostCategories() {
           <div>مدیریت خواب</div>
           <div className="text-sm font-normal">الگوهای خواب و استراحت</div>
         </div>
-      <div className="absolute -bottom-[20%] right-[6%] text-4xl w-10 h-10 bg-white rounded-full">۱۰</div>
+      <div className="absolute -bottom-6 right-4 text-4xl w-10 h-10 bg-white rounded-full">۱۰</div>
       </Link>
     </div>
   );
